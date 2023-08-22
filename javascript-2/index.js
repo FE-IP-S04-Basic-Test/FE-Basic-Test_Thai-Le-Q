66d4ddf9eaf37e10d793cef65844d9a00e3b1c69
cmvn-thaile <thai.le@classmethod.vn>
2023-08-22 12:17:42 +0700

    fix bug js task 2

--- javascript-2/index.js
+++ javascript-2/index.js
@@ -1,34 +1,26 @@
-function mergeProducts(arr1, arr2) {
-  const merged = {};
-  const result = [];
-
-  arr1.forEach((item) => {
-    if (!merged[item.id]) {
-      merged[item.id] = item.quantity;
-    } else {
-      merged[item.id] += item.quantity;
-    }
-  });
-
-  arr2.forEach((item) => {
-    if (merged[item.id]) {
-      result.push({ id: item.id, quantity: merged[item.id] + item.quantity });
-    }
-  });
-
-  return result;
-}
-
 const arr1 = [
-  { id: '1', quantity: 2 },
-  { id: '2', quantity: 2 },
-  { id: '3', quantity: 3 },
-  { id: '1', quantity: 3 }
+  { id: "1", quantity: 2 },
+  { id: "3", quantity: 2 },
 ];
 
 const arr2 = [
-  { id: '1', quantity: 2 },
-  { id: '3', quantity: 2 }
+  { id: "1", quantity: 2 },
+  { id: "2", quantity: 2 },
+  { id: "3", quantity: 3 },
+  { id: "1", quantity: 3 },
 ];
+function mergeProducts(arr1, arr2) {
+  const result = [];
+  arr1.forEach((product1) => {
+    const product2 = arr2.find((p) => p.id === product1.id);
+    if (product2) {
+      result.push({
+        id: product1.id,
+        quantity: product1.quantity + product2.quantity,
+      });
+    }
+  });
+  return result;
+}
 
 console.log(mergeProducts(arr1, arr2));
